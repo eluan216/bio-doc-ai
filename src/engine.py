a4ac7dba8a92edaef3ea2f2bf293ebaedf80a83f
eluan216 <134099412+eluan216@users.noreply.github.com>
2026-01-29 01:41:29 +0100

    Fix: Update Document import from langchain_core.documents

--- src/engine.py
+++ src/engine.py
@@ -12,7 +12,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.schema import Document
+from langchain_core.documents import Document
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
